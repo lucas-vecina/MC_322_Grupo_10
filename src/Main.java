b75841900a945af005e5528d177203eb105b1a84
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-03 21:35:33 -0300

    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("OI");
 		System.out.println("OI");
-		System.out.println("hello");
+		System.out.println("test");
 
 	}
 
