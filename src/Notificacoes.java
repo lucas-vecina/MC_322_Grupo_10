7f1847458b48aa716768237d0c00f185c8c6d04b
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-05 17:30:14 -0300

    Inicializacao Enum Notificaoes

---
+++ src/Notificacoes.java
@@ -0,0 +1,18 @@
+public enum Notificacoes {
+	NOVA_MENSAGEM("Você recebeu uma nova mensagem!"),
+	NOVA_ATIVIDADE("Você tem uma nova atividade para entrega!"),
+	NOVO_AMIGO("Você tem um novo amigo!"),
+	INICIO_MONITORIA("Uma monitoria acabou de começar!"),
+	INICIO_AULA("Uma nova aula foi postada"),
+	NOVA_RESPOSTA("Responderam uma dúvida que você fez!");
+	
+	private final String notificacao;
+	
+	Notificacoes(String notificacao) {
+		this.notificacao = notificacao;
+	}
+
+	public String getNotificacao() {
+		return notificacao;
+	}
+}
