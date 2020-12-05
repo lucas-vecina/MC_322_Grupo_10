81b1b7e991b7c2b728e7a7c5a76f32aa341bec4a
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-05 17:08:37 -0300

    Criação Enum Cor

---
+++ src/Cor.java
@@ -0,0 +1,19 @@
+public enum Cor {
+	VERMELHO ("vermelho"),
+	AZUL ("azul"),
+	AMARELO ("amarelo"),
+	LARANJA ("laranja"),
+	VERDE ("verde"),
+	ROXO ("roxo"),
+	PRETO ("preto");
+	
+	private final String cor;
+	
+	Cor(String cor){
+		this.cor = cor;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+}
