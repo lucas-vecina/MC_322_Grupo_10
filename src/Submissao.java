7911bc691e2cf7015a42c81164b55531a64e0a00
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-10 19:08:43 -0300

    Estabelecimento de vínculo com AtividadeGrupo e inserção de comentários

--- src/Submissao.java
+++ src/Submissao.java
@@ -1,13 +1,28 @@
 
 public class Submissao {
-	private Aluno aluno;
-	private String arquivo;
-	private double nota;
 	
+	/* Essa é uma classe de apoio as classe AtividadeIndividual e AtividadeGrupo.
+	 * Ela possibilita um correspondênicia direta entre o arquivo de envio e a nota com o aluno, seja ele individual
+	 * ou em grupo.
+	 */
+	
+	private Aluno aluno;
+	private Grupo equipe;
+	private String arquivo;
+	private double nota = -1.0;
+	
+	// Construtor sobrecarregado invocado por AtividadeIndividual
 	public Submissao(Aluno aluno, String arquivo) {
 		this.aluno = aluno;
 		this.arquivo = arquivo;
-		this.nota = -1.0;
+	}
+	
+	/* Construtor sobrecarregado invocado por AtividadeGrupo. 
+	 * Notar que arquivo não é passado de inicio. Isso porque o grupo é estabelecido previamente e só depois a 
+	 * atividade é submetida (alterada pelo set())
+	 */
+	public Submissao(Grupo equipe) {
+		this.equipe = equipe;
 	}
 	
 	public Aluno getAluno() {
@@ -29,10 +44,19 @@ public class Submissao {
 		this.nota = nota;
 	}
 
+	public Grupo getEquipe() {
+		return equipe;
+	}
+
+	public void setEquipe(Grupo equipe) {
+		this.equipe = equipe;
+	}
+
 	public String toString() {
 		String out = "\n";
-		out+= "! Aluno: " + getAluno().getNome() + " [" + getAluno().getRa() + "]" + "\n";
-		out+= "! Arquivo: " + getArquivo() + "\n";
+		out+= (getAluno() != null ? "! Aluno: " + getAluno().getNome() + " [" + getAluno().getRa() + "]" + "\n" : "");
+		out+= (getEquipe() != null ? "! Grupo: " + getEquipe() + "\n" : "");
+		out+= (getArquivo() != null ? "! Arquivo: " + getArquivo() + "\n" : "");
 		out+= "! " + (getNota() != -1 ? "Nota: " + getNota() : "Nota ainda não atribuída") + "\n";
 		
 		return out;
