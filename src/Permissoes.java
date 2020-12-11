0a9ffbc85db7b9295e2674d5f0434ffe3aa6dc9f
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-11 17:56:22 -0300

    Permissoes foi aperfeiçoado

--- src/Permissoes.java
+++ src/Permissoes.java
@@ -1,5 +1,21 @@
 public enum Permissoes {
-	PUBLIC,
-	PRIVATE,
-	DEFAULT;
+	PUBLIC (1, "Permissão Pública: Qualquer um pode visualizar grande parte das suas informações e lhe enviar mensagens."),
+	DEFAULT (2, "Permissão Default: Qualquer um pode visualizar algumas de suas informações e lhe enviar mensagens."),
+	PRIVATE (3, "Permissão Privada: Somente amigos podem visualizar algumas de suas informações e lhe enviar mensagens.");
+	
+	int chave;
+	String descricao;
+	
+	private Permissoes(int chave, String descricao) {
+		this.chave = chave;
+		this.descricao = descricao;
+	}
+
+	public int getChave() {
+		return chave;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }
