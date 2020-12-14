3f4c5b8215d5b1ddfabde208df2e9e39e788b24f
Lucas Vecina <lucas.vecina@gmail.com>
2020-12-14 00:19:42 -0300

    Agora o grupo aparece na lista de grupos do usuario

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList; 
 
-public class Grupo {
+// Permite unir diferentes usuarios
+public class Grupo {	
 	private String nomeGrupo;
 	private ArrayList<Usuario> grupo; 
 	private boolean status; 
@@ -8,9 +9,6 @@ public class Grupo {
 	public Grupo(String nomeGrupo, ArrayList<Usuario> grupo) {
 		this.nomeGrupo = nomeGrupo;
 		this.grupo = grupo; 
-		for(Usuario membro:grupo) {
-			membro.getGrupos().add(this);
-		}
 		status = true;
 	}
 	
@@ -44,21 +42,33 @@ public class Grupo {
 		this.status = status;
 	}
 	
-	public void adicionarUsuario(Usuario integrante, Usuario forasteiro) {
+	public void adicionarGrupo() {
+		for(Usuario membro:getGrupo())
+			membro.getGrupos().add(this);
+	}
+	
+	public boolean adicionarUsuario(Usuario integrante, Usuario forasteiro) {
 		if(grupo.contains(integrante)) {
 			if(!grupo.contains(forasteiro)) {
 				grupo.add(forasteiro); 
 				forasteiro.getGrupos().add(this);
+				
+				return true;
 			}
 		}
+		return false;
 	}
 	
-	public void removerUsuario(Usuario integrante, Usuario removido) {
+	public boolean removerUsuario(Usuario integrante, Usuario removido) {
 		if(grupo.contains(integrante)) {
 			if(grupo.contains(removido)) {
 				grupo.remove(removido); 
+				removido.getGrupos().remove(this);
+				
+				return true;
 			}
 		}
+		return false;
 	}
 	
 	public void eliminarGrupo(Usuario integrante) {
@@ -72,11 +82,11 @@ public class Grupo {
 	}
 	
 	public String toString() {
-		String out = "Nome do grupo:" + nomeGrupo;
+		String out = "\n* Nome do grupo:" + nomeGrupo;
 		out = out + "\nPessoas:";
 		for(Usuario pessoa:grupo) 
 			out = out + "\n" + pessoa.getNome() + ", Id:" + pessoa.getId(); 
-		out = out + "\nStatus:" + status; 
+		out = out + "\nStatus:" + status + "\n"; 
 		return out; 
 	}
 }
